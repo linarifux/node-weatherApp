43b6709237b793e62acf7b93bc7e3e4e48d25add
linarifux <linarifux@gmail.com>
2021-06-01 03:50:46 +0600

    make it better

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 
 const app = express()
+
+const port = process.env.PORT || 5000
 app.use(express.static(__dirname + '/public'));
-app.listen(8000, ()=> {
+app.listen(port, ()=> {
     console.log('server started....');
 })
 
@@ -22,6 +24,7 @@ app.get('/', (req,res)=> {
 })
 
 app.post('/result', async (req,res) => {
+    
     const city = req.body.city
     const options = {'method':'POST'}
     const API_KEY = 'ba94c4d1e85eb7c3f7ead9e2427a4cd1'
@@ -29,13 +32,11 @@ app.post('/result', async (req,res) => {
     const response = await fetch(url, options)
         .then(res => res.json())
         .catch(err => console.log(err))
-    
-    console.log("Response: ", response.main.temp);
+
     const temp = (response.main.temp - 273.15).toFixed(2)
     const cityName = response.name
     const wind = response.wind.speed
     const feelsLike = (response.main.feels_like - 273.15).toFixed(2)
-    // res.json(temp)
 
     res.render('result', {wind,feelsLike,temp,cityName,siteInformation})
 })
